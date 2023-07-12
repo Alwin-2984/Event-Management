69e709a0569f21fe4ca9e48e7fd63c446743bbc1
alwin.kc <alwin.kc@innovaturelabs.com>
2023-07-12 13:49:36 +0530

    starting

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Dashboard from "./screens/User/Dashboard"
+function App() {
+  return (
+    <>
+      <div className="w-full">
+      <Dashboard/>
+      </div>
+    </>
+  );
+}
+
+export default App;
