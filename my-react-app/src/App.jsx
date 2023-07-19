0e373ecb84121c614fe8cdc9271359f562a1ba13
alwin.kc <alwin.kc@innovaturelabs.com>
2023-07-19 10:15:28 +0530

    404 page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './screens/User/Dashboard';
+import NotFound from './screens/Components/NotFound404'; // Import your custom 404 component
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
           <Route path="/dashboard" element={<Dashboard />} />
+
           {/* Add more routes as needed */}
+          <Route path="*" element={<NotFound />} /> {/* 404 page */}
         </Routes>
       </BrowserRouter>
     </div>
